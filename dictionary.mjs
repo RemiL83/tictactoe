68b78c12791e7bf3a78423b7b37efd6cf89c14f8
RemiL83 <reloite@gmail.com>
2024-12-04 18:22:50 +0100

    Fjernet overflødige filer.

--- dictionary.mjs
+++
@@ -1,9 +0,0 @@
-const dictionary = {
-    restartSlutt: `Skriv \`r\` for å starte på nytt eller \`q\` for å avslutte.`,
-    spillerTur: `Det er spiller ${spillerNavn()} sin tur`,
-    instruks: `Hvor setter du merket ditt? Skriv rad,kolonne, for eksempel 1,2.`,
-    vinner:`Vinneren er ${spillerNavn(vinner)}`,
-    uavgjort: `Det ble uavgjort`
-}
-
-export default dictionary
