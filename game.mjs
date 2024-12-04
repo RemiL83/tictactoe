792fa9ddff74191593fa21ff9cd7dc128a314722
RemiL83 <reloite@gmail.com>
2024-12-04 14:43:51 +0100

    R for å spille på nytt. Q for å avslutte.

--- game.mjs
+++ game.mjs
@@ -8,59 +8,121 @@ const rl = readlinePromises.createInterface({
 
 import ANSI from "./ANSI.mjs"
 
-let vinner = 0;
-let brett = 
-[
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-];
-
-//#region Logikken for spillet tre på rad. --------------------------------------------------------
-
 const spiller1 = 1;
 const spiller2 = -1;
-
-let resultatAvSpill = "";
 let spiller = spiller1;
-let isGameOver = false
+let resultatAvSpill = "";
 
-while (isGameOver == false) {
+while (true) {
+    await hovedSpill();
+    const sluttvalg = await visMeny();
+    if (sluttvalg === 'r') {
+    } else {
+        console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
+        console.log(`Takk for spillet. Velkommen igjen.`);
+        process.exit();
+    }
+}
+
+//#endregion---------------------------------------------------------------------------------------
+
+function spillerNavn(sp = spiller) {
+    if (sp == spiller1) {
+        return "spiller 1(X)";
+    } else {
+        return "spiller 2(O)";
+    }
+}
+
+function visBrett(brett) {
+
+    let visningAvBrett = "     1     2     3\n";
+    visningAvBrett += "  ╔═════╦═════╦═════╗\n";
+
+    for (let i = 0; i < brett.length; i++) {
+        const rad = brett[i];
+        let visningAvRad = `${i + 1} ║`;
+        for (let j = 0; j < rad.length; j++) {
+            let verdi = rad[j];
+            if (verdi == 0) {
+                visningAvRad += `     ║`;
+            } else if (verdi == spiller1) {
+                visningAvRad += ANSI.COLOR.GREEN + `  X` + ANSI.COLOR_RESET + `  ║`;
+            } else {
+                visningAvRad += ANSI.COLOR.RED + `  O` + ANSI.COLOR_RESET + `  ║`;
+            }
+        }
+        visningAvRad += "\n";
+        visningAvBrett += visningAvRad;
+
+        if (i < brett.length - 1) {
+            visningAvBrett += "  ╠═════╬═════╬═════╣\n";            
+        } else {
+            visningAvBrett += "  ╚═════╩═════╩═════╝\n";
+        }
+    }
+
+    console.log(visningAvBrett);
+}
+
+function byttAktivSpiller() {
+    spiller = spiller * -1;
+}
+
+async function hovedSpill() {
+
+    let vinner = 0;
+    let brett = 
+    [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    
+    let isGameOver = false;
+
+    while (isGameOver == false) {
+
+        console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
+        visBrett(brett);
+        console.log(`Det er ${spillerNavn(spiller)} sin tur.\n`);
+
+        let rad = -1;
+        let kolonne = -1;
+
+        do {
+            let pos = await rl.question(`Skriv 'rad,kolonne' (f.eks: 1,1), og trykk enter for å sette merket ditt. Skriv 'q' for å avslutte.\n`);
+            if (pos.trim().toLowerCase() === 'q') {
+                console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
+                console.log(`Velkommen igjen.`);
+                process.exit();
+            }
+            [rad, kolonne] = pos.split(",")
+
+            rad = rad - 1;
+            kolonne = kolonne - 1;
+
+        } while (brett[rad][kolonne] != 0)
+
+        brett[rad][kolonne] = spiller;
+
+        vinner = harNoenVunnet(brett);
+        if (vinner != 0) {
+            resultatAvSpill = `Vinneren er ${spillerNavn(vinner)}.\n`;
+            isGameOver = true;
+
+        } else if (erSpilletUavgjort(brett)) {
+            resultatAvSpill = "Det ble uavgjort.\n";
+            isGameOver = true;
+        }
+        byttAktivSpiller();
+    }
 
     console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
     visBrett(brett);
-    console.log(`Det er spiller ${spillerNavn()} sin tur`);
+    console.log(resultatAvSpill);
 
-    let rad = -1;
-    let kolonne = -1;
-
-    do {
-        let pos = await rl.question("Hvor setter du merket ditt? Skriv rad,kolonne, for eksempel 1,2.\n");
-        [rad, kolonne] = pos.split(",")
-        rad = rad - 1;
-        kolonne = kolonne - 1;
-
-    } while (brett[rad][kolonne] != 0)
-
-    brett[rad][kolonne] = spiller;
-
-    vinner = harNoenVunnet(brett);
-    if (vinner != 0) {
-        isGameOver = true;
-        resultatAvSpill = `Vinneren er ${spillerNavn(vinner)}`;
-    } else if (erSpilletUavgjort(brett)) {
-        resultatAvSpill = "Det ble uavgjort";
-        isGameOver = true;
-    }
-    byttAktivSpiller();
 }
-
-console.log(ANSI.CLEAR_SCREEN, ANSI.CURSOR_HOME);
-visBrett(brett);
-console.log(resultatAvSpill);
-process.exit();
-
-//#endregion---------------------------------------------------------------------------------------
 
 function harNoenVunnet(brett) {
 
@@ -114,45 +176,11 @@ function erSpilletUavgjort(brett) {
     return true;
 }
 
-function visBrett(brett) {
-
-    let visningAvBrett = "     1     2     3\n";
-    visningAvBrett += "  ╔═════╦═════╦═════╗\n";
-
-    for (let i = 0; i < brett.length; i++) {
-        const rad = brett[i];
-        let visningAvRad = `${i + 1} ║`;
-        for (let j = 0; j < rad.length; j++) {
-            let verdi = rad[j];
-            if (verdi == 0) {
-                visningAvRad += `     ║`;
-            } else if (verdi == spiller1) {
-                visningAvRad += ANSI.COLOR.GREEN + `  X` + ANSI.COLOR_RESET + `  ║`;
-            } else {
-                visningAvRad += ANSI.COLOR.RED + `  O` + ANSI.COLOR_RESET + `  ║`;
-            }
-        }
-        visningAvRad += "\n";
-        visningAvBrett += visningAvRad;
-
-        if (i < brett.length - 1) {
-            visningAvBrett += "  ╠═════╬═════╬═════╣\n";            
-        } else {
-            visningAvBrett += "  ╚═════╩═════╩═════╝\n";
+async function visMeny() {
+    while (true) {
+        const valg = await rl.question(`Skriv 'r' for å spille på nytt eller 'q' for å avslutte.\n`);
+        if (valg === 'r' || valg === 'q') {
+            return valg;
         }
     }
-
-    console.log(visningAvBrett);
-}
-
-function spillerNavn(sp = spiller) {
-    if (sp == spiller1) {
-        return "Spiller 1(X)";
-    } else {
-        return "Spiller 2(O)";
-    }
-}
-
-function byttAktivSpiller() {
-    spiller = spiller * -1;
 }
